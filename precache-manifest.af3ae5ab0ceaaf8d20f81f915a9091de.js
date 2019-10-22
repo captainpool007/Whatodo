bc4196fcedf53ae3fa3494e8637c8fa37e4665e0
captainpool007 <startupdevta@gmail.com>
2019-10-22 23:13:49 +0530

    Updates

---
+++ precache-manifest.af3ae5ab0ceaaf8d20f81f915a9091de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253f9d11670f49ae40e040d055ce8207",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "75b831960cb998da6fad",
+    "url": "/todoapp/static/css/2.c2bf9dd8.chunk.css"
+  },
+  {
+    "revision": "44fa15ac0e59e6fa026f",
+    "url": "/todoapp/static/css/main.bc78fb55.chunk.css"
+  },
+  {
+    "revision": "75b831960cb998da6fad",
+    "url": "/todoapp/static/js/2.92b1e755.chunk.js"
+  },
+  {
+    "revision": "44fa15ac0e59e6fa026f",
+    "url": "/todoapp/static/js/main.4f902d8f.chunk.js"
+  },
+  {
+    "revision": "1aead3b32398b3b33e15",
+    "url": "/todoapp/static/js/runtime-main.78970db3.js"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/todoapp/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/todoapp/static/media/Roboto-Regular.3e1af3ef.ttf"
+  }
+]);
